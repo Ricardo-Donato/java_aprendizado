9991193353e4434dd79d1ca23d1db1248587ed82
Ricardo-Donato <pdonatodasilva@gmail.com>
2025-10-10 20:09:29 -0300

    Incrementação e decrementação em Java

---
+++ src/aula_10_10/Incrementacao.java
@@ -0,0 +1,19 @@
+package aula_10_10;
+
+/**
+ *
+ * @author Pedro
+ */
+public class Incrementacao { // e Decrementação
+
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("a: " + a);
+        System.out.println("++a: " + ++a);
+        System.out.println("a++: " + a++);
+        System.out.println("a: " + a);
+        System.out.println("--a: " + --a);
+        System.out.println("a--: " + a--);
+        System.out.println("a: " + a);
+    }
+}
